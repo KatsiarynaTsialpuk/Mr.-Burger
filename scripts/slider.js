8f57a29f7adabc100806b3192e2435b17fb7a915
Katia <katya@spiralscout.com>
2018-05-18 17:10:44 +0300

    F add slider js

---
+++ scripts/slider.js
@@ -0,0 +1,32 @@
+const left = document.querySelector('#left-arrow');
+const right = document.querySelector('#right-arrow');
+const items = document.querySelector('.slider__list');
+const listItem = document.querySelector('.slider__content');
+const computed = getComputedStyle(items);
+
+right.addEventListener('click', function (e) {
+    e.preventDefault();
+    let curentRight = parseInt(computed.right);
+
+    if (!curentRight) {
+        curentRight = 0;
+    }
+
+    if (curentRight < 3520) {
+        items.style.right = curentRight + 880 + 'px';
+        listItem.classList.add('slider__content_active');
+    }
+});
+
+left.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let curentRight = parseInt(computed.right);
+    if (!curentRight) {
+        curentRight = 0;
+    }
+
+    if (curentRight > 0) {
+        items.style.right = curentRight - 880 + 'px';
+    }
+})
