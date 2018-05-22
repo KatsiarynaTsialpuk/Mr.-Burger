d4835b75929cf374411da6a2efc89de39e4d4215
Katia <katya@spiralscout.com>
2018-05-22 19:58:19 +0300

    F add popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,30 +1,27 @@
 (function () {
-    const content = document.querySelector('.review__content');
-    content.addEventListener('click', function (event) {
+    const reviewList = document.querySelector('.reviews__list');
+    reviewList.addEventListener('click', function (event) {
         if (event.target.classList.contains('order-btn')) {
             event.preventDefault();
 
             const eventThis = event.target;
-            const popup = eventThis.nextElementSibling;
+            const popupText = eventThis.previousElementSibling;
+            const text = popupText.textContent;
+            const itemTitle = popupText.previousElementSibling;
+            const title = itemTitle.textContent;
+            const popupTitle = document.querySelector('.popup__title');
+            const popupParagraph = document.querySelector('.popup__text');
+            popupParagraph.innerHTML = text;
+            popupTitle.innerHTML = title;
+            const popup = document.querySelector('.popup');
+            popup.classList.add('popup__active');
 
-            if (!popup.classList.contains('popup__active')) {
-                popup.classList.add('popup__active');
-                popup.style.display = 'flex';
-            }
-        }
-    });
-
-    content.addEventListener('click', function (event) {
-        if (event.target.classList.contains('popup__close')) {
+        } else if (event.target.classList.contains('popup__close')) {
             event.preventDefault();
 
-            const eventThis = event.target;
-            const popup = eventThis.nextElementSibling;
-
-            if (popup.classList.contains('popup__active')) {
-                popup.classList.remove('popup__active');
-                popup.style.display = 'none';
-            }
+            const popup = document.querySelector('.popup');
+            popup.classList.remove('popup__active');
         }
-    })
+
+    });
 })();
